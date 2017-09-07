debd472588ad473b53d517d0c8dae8bf970631a6
Mon Maldonado <monterdi@gmail.com>
2017-09-07 14:02:18 +0200

    Integración de material y primera versión de gitlba-ci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {MdToolbarModule} from '@angular/material';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
