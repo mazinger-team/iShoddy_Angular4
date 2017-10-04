a5bc94ced81cbbf6e2b18e61de7467eddb9ba2d4
Mon Maldonado <monterdi@gmail.com>
2017-10-04 15:26:04 +0200

    Environment de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://www.mocky.io/v2/59badac50f00005104622a16'
+  url: 'http://ec2-54-202-209-58.us-west-2.compute.amazonaws.com/api/v1/'
 };
